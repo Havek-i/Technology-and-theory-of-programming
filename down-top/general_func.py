11b9bf0bec65e81a2630afacfd1687247f502b41
Havek-i <ivavasilev2004@gmail.com>
2025-11-24 14:59:18 +0700

    Конец 3-й итерации (ПР down-top)

--- down-top/general_func.py
+++ down-top/general_func.py
@@ -1,39 +1,57 @@
+import random
+import string
 import helper_func as hf
 
 def start_menu() -> None:
     '''Функция, которая отображет меню'''
 
-    print('''
+    menu = '''
     =============================
     1. Ввести данные.
     2. Выполнить алгоритм.
     3. Вывести результат.
     0. Завершение работы прграммы.
     =============================
-    ''')
+    '''
+    print(menu)
 
 
 def crypto(string: str, shift: int) -> str:
     '''Функция, которая реализует аглоритм шифра Виженера'''    
     
     print("Начало алгоритма...")
+    result = []
+    for char in string:
+        base = ord('a')
+        # Вычисляем новую позицию символа с учетом сдвига и закольцованности алфавита
+        new_char = chr((ord(char) - base + shift) % 26 + base)
+        result.append(new_char)
     print("Алгоритм завершил работу")
     
-    return 'crypto-string'
+    return ''.join(result)
 
-def input_params():
+def input_params() -> tuple[str, int]:
     '''Функция, которая принимает исходные данные'''
     
-    input("Введите данные: ")
+    string, shift = input("Введите данные: ").strip().split(' ')
+    string, shift = hf.normalize_input_data(string, shift) #Использование вспомогательной функции
     print("Данные были введены!")
+    return (string, shift)
     
-def generating_params():
+def generating_params() -> tuple[str, int]:
     '''Функция, которая генерирует значения случайным образом'''
 
-    print(('generation_string', 7))
+    length = random.randrange(1, 20)
+    shift = random.randrange(1, 20)
+
+    random_string = ''.join(random.choices(string.ascii_uppercase, k=length))
+
+    print((random_string, shift))
+    return (random_string, shift)
     
 
-def result():
+def result(string: str) -> None:
     '''Функция, которая вывод результат'''
     
-    print('result')
+    string = string.capitalize()
+    print(string)
--- down-top/main.py
+++ down-top/main.py
@@ -1,19 +1,48 @@
-
-import helper_func as hf
 import general_func as gf
+import helper_func as hf
 
 def main():
+    #Добавление глобальных переменных для сохранения данных
+    global data
+    global result
+    data, result = None, None
 
-    #Готовые вспомогательные функции
-    print(hf.normalize_input_data('HdfjjweWgnkndsg', '10'))
-    hf.submenu()
+    while True:
+        gf.start_menu()
+        item = int(input('Введите пункт меню: ').strip())
+        
+        #Выполнение выбранных пунктов меню
+        match item:
+            case 0:
+                break
 
-    #Шаблоны основных функций
-    gf.start_menu()
-    gf.crypto('string', 3) 
-    gf.input_params()
-    gf.generating_params()
-    gf.result()
+            case 1:
+                sub_item = hf.submenu()
+                if sub_item == 1:
+                    data = gf.input_params()
+                elif sub_item == 2:
+                    data = gf.generating_params()
+                else:
+                    print("Вы ввели некорректный подпункт, попробуйте ещё раз.")
+                    continue
+                
+                result = None
+
+            case 2:
+                if data:
+                    result = gf.crypto(string=data[0], shift=data[1])
+                else:
+                    print("Вы не ввели данные. Попробуйте для начала выполнить пункт 1.")
+
+            case 3:
+                if result:
+                    gf.result(result)
+                else:
+                    print("Вы не выполнили алгоритм. Попробуйте выполнить для начала пункт 2")
+            
+            case _:
+                print("Такой команды нет в списке. Попробуйте ещё раз!")
+                continue
 
 
 if __name__ == '__main__':
