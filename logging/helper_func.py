a9aa0b742746ad007bbe49d9599d6fc54fc6cee9
Havek-i <ivavasilev2004@gmail.com>
2025-11-27 09:34:51 +0700

    Добавление возможности логирования серверных действий

---
+++ logging/helper_func.py
@@ -0,0 +1,53 @@
+from logg import server_logger
+
+def normalize_input_data(string: str, shift: str) -> tuple[str, int]:
+    '''Функция, которая обрабатывает входные данные
+    
+    Arguments
+    ---------
+        string : str
+            Исходная строка
+        shift : str 
+            Величина сдвига
+    
+    Returns
+    -------
+        tuple : [str, int]
+            Кортеж, состоящий из преобразованных строки и величины сдвига
+    '''
+    
+    server_logger.info("Нормализация входных данных")
+
+    string = string.lower()
+    shift = int(shift)
+    
+    server_logger.info(f"Данные успешно нормализованы: {string}, {shift}")
+
+    return (string, shift)
+
+def submenu(submenu: str = None) -> int:
+    '''Функция, которая показывает подменю и обрабатывает выбранный пункт
+    
+    Arguments
+    ---------
+        Submenu : str | None
+            Исходная строка
+    
+    Returns
+    -------
+        func : function
+            Функция, запрашивающая у Пользователя пункт submenu
+    '''
+    
+    server_logger.info("Вызов подменю")
+
+    if submenu is None:
+        submenu = '''
+        \t============================
+        \t1. Самостояльный ввод данных
+        \t2. Случайная генерация
+        \t============================
+        '''
+
+    print(submenu)
+    return int(input("\t\tВыберите пункт: "))
