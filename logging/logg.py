f719766172bc374830488195ac5d5890358deb71
Havek-i <ivavasilev2004@gmail.com>
2025-11-27 09:32:02 +0700

    Улучшение отоборажения логов

--- logging/logg.py
+++ logging/logg.py
@@ -5,13 +5,13 @@ formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
 # Клиентский логгер
 client_logger = logging.getLogger('_client_')
 client_logger.setLevel(logging.INFO)
-client_handler = logging.FileHandler("client.log")
+client_handler = logging.FileHandler("logging/client.log", encoding='utf-8')
 client_handler.setFormatter(formatter)
 client_logger.addHandler(client_handler)
 
 # Серверный логгер
 server_logger = logging.getLogger('_server_')
 server_logger.setLevel(logging.INFO)
-server_handler = logging.FileHandler("server.log")
+server_handler = logging.FileHandler("logging/server.log", encoding='utf-8')
 server_handler.setFormatter(formatter)
 server_logger.addHandler(server_handler)
