17fd82c0313ab5a1c3eb2a2c374545caffd1aa62
Havek-i <ivavasilev2004@gmail.com>
2025-11-28 14:34:48 +0700

    Добавлено логгирование для сервера и клиента

---
+++ exception-v2/logg.py
@@ -0,0 +1,17 @@
+import logging
+
+formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+
+# Клиентский логгер
+client_logger = logging.getLogger('_client_')
+client_logger.setLevel(logging.INFO)
+client_handler = logging.FileHandler("exception-v2/log/client.log", encoding='utf-8')
+client_handler.setFormatter(formatter)
+client_logger.addHandler(client_handler)
+
+# Серверный логгер
+server_logger = logging.getLogger('_server_')
+server_logger.setLevel(logging.INFO)
+server_handler = logging.FileHandler("exception-v2/log/server.log", encoding='utf-8')
+server_handler.setFormatter(formatter)
+server_logger.addHandler(server_handler)
